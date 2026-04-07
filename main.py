d677c347b5b676cfbd591f10d60a2f31a116709f
Ayushs135 <ayushs01032005@gmail.com>
2026-04-07 14:00:27 +0530

    Added initial file

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+
+conn = MongoClient(REMOVED)
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    docs = conn.notes.notes.find({})
+    new_docs = []
+    for doc in docs:
+        new_docs.append({"id": doc["_id"], "note": doc["note"]})
+    return templates.TemplateResponse(request=request, name="index.html", context={"request": request, "new_docs": new_docs})
+
+@app.get("/items/{item_id}")
+async def read_item_by_id(item_id: int):
+    return {"item_id": item_id}
